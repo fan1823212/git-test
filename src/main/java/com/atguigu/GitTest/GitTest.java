dea36be9991124d303a2f521dff1239658e0e0a5
@fan <2328532553@qq.com>
2022-11-14 20:36:53 +0800

    第2次提交，决绝乱码问题

--- src/main/java/com/atguigu/GitTest/GitTest.java
+++ src/main/java/com/atguigu/GitTest/GitTest.java
@@ -2,6 +2,7 @@ package com.atguigu.GitTest;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("第一次提交git");
+        System.out.println("这是个修理bug的分支");
+        System.out.println("第二次提交git，解决乱码问题");
     }
 }
